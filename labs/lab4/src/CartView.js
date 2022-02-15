4e6c3a3342acee764443f2feba14bc228f9383fb
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2022-02-15 17:08:48 +0100

    lab4: Add order placement functionality

--- labs/lab4/src/CartView.js
+++ labs/lab4/src/CartView.js
@@ -4,16 +4,31 @@ class CartView extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.sendOrder = this.sendOrder.bind(this);
   }
 
   render() {
     return (
-      <ul>
-        {this.props.cart.map(salad =>
-          <li key={salad.uuid}> {salad.toString()} </li>
-        )}
-      </ul>
+      <div>
+        <ul>
+          {this.props.cart.map(salad =>
+            <li key={salad.uuid}> {salad.toString()} </li>
+          )}
+        </ul>
+        <button className='btn btn-primary' onClick={this.sendOrder}> Order </button>
+      </div>
     )
+  }
+
+  sendOrder() {
+    const order = this.props.cart.map(salad => Object.keys(salad.ingredients))
+    fetch(
+      'http://localhost:8080/orders/',
+      { method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(order)
+      }).then(res => res.json())
+        .then(data => alert(JSON.stringify(data)))
   }
 
 }
